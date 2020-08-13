d68e4b8f9fab130078f4ff28c309663451019083
= <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2020-08-13 16:17:47 -0700

    Completed 2 applications

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,22 @@
+# # Lookup Table
+
+# For expensive operations, caching the results in a lookup table speeds
+# future queries.
+
+# The lookup table can be built in advance by iterating over all values in
+# the _domain_ of the function and recording the results.
+
+# Or, more lazily, can be build as the individual values are passed in.
+
+# Modify the code in this directory to build a lookup table so that it can
+# finish running in under a minute.
+
+# There's no test file for this. It's counting to 50,000, so if it
+# finishes before you give up, then you're golden.
+
+
 # Your code here
+import random, math, time
 
 
 def slowfun_too_slow(x, y):
@@ -9,13 +27,19 @@ def slowfun_too_slow(x, y):
 
     return v
 
+cache = {}
+cache_pow = {}
+cache_fact = {}
+cache_intd = {}
+cache_mod = {}
+v = None
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
-
 
 
 # Do not modify below this line!
--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,32 @@
+# # No Duplicates
+
+# Input: a string of words separated by spaces. Only the letters `a`-`z`
+# are utilized.
+
+# Output: the string in the same order, but with subsequent duplicate
+# words removed.
+
+# There must be no extra spaces at the end of your returned string.
+
+# The solution must be `O(n)`.
+
+
 def no_dups(s):
     # Your code here
 
-
+    key_str = ""
+#  .split turns string into a list
+    words = s.split()
+    # create dictionary
+    dict_keys = dict() 
+    # going through words
+    for item in words:
+        # logic for only showing words that have not appeared
+        if item not in dict_keys.keys():
+            dict_keys[item] = item 
+            key_str += item + ' '
+     
+    return key_str.strip()  # removes beg & end spaces in string
 
 if __name__ == "__main__":
     print(no_dups(""))
